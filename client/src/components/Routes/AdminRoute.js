c210f017a0fd87000986514bc49a46cd17bfb3ba
George Tay <georgetay71@gmail.com>
2025-02-14 23:08:42 +0800

    test: implement AdminRoute tests

--- client/src/components/Routes/AdminRoute.js
+++ client/src/components/Routes/AdminRoute.js
@@ -1,8 +1,8 @@
-import { useState,useEffect } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "../../context/auth";
 import { Outlet } from "react-router-dom";
-import axios from 'axios';
-import { set } from "mongoose";
+import axios from "axios";
 import Spinner from "../Spinner";
 
 export default function AdminRoute(){
