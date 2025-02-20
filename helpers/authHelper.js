b7b405ced9a2e1a6f1e419e11858df7be214cf8e
bernice <bernicetoh2003@gmail.com>
2025-02-20 14:11:30 +0800

    refactor authhelper comparepassword

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -1,16 +1,20 @@
 import bcrypt from "bcrypt";
 
 export const hashPassword = async (password) => {
-    try {
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        return hashedPassword;
-
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const comparePassword = async (password,hashedPassword) => {
-    return bcrypt.compare(password, hashedPassword);
-}
+export const comparePassword = async (password, hashedPassword) => {
+  try {
+    const match = await bcrypt.compare(password, hashedPassword);
+    return match;
+  } catch (error) {
+    console.log(error);
+  }
+};
