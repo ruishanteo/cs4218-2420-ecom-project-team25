363e135f166a94d2ec34e6bd429cd477a77adbc3
George Tay <georgetay71@gmail.com>
2025-02-23 21:52:40 +0800

    chore: enhance Private route test cases

--- client/src/components/Routes/Private.test.js
+++ client/src/components/Routes/Private.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Spinner from '../Spinner';
 import axios from 'axios';
 import { useAuth } from '../../context/auth';
 import PrivateRoute from './Private';
+import { render, waitFor } from '@testing-library/react';
 
 jest.mock('axios');
 jest.mock('../../context/auth', () => ({
@@ -19,7 +20,24 @@ jest.mock('react', () => ({
   useState: jest.fn(() => [false, jest.fn()]),
 }));
 
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(),
+  useLocation: jest.fn(),
+}));
+
+let consoleSpy;
+
 describe('Private', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+  });
+
+  afterAll(() => {
+    consoleSpy.mockRestore();
+  });
+
   it('should return Spinner by default if no auth token is present', () => {
     expect(PrivateRoute()).toStrictEqual(<Spinner path='' />);
   });
@@ -37,5 +55,24 @@ describe('Private', () => {
     useState.mockReturnValueOnce([true, jest.fn()]);
 
     expect(PrivateRoute()).toStrictEqual(<Outlet />);
+  });
+
+  it('should not crash if get errors out', async () => {
+    const err = new Error('Failed to query auth status');
+    axios.get.mockRejectedValueOnce(err);
+
+    useAuth.mockReturnValueOnce([{ token: 'token' }, jest.fn()]);
+
+    // force the render of the Outlet component here to test exceptions
+    useState.mockReturnValueOnce([true, jest.fn()]);
+    render(<PrivateRoute />);
+
+    await waitFor(() => {
+      expect(axios.get).toHaveBeenCalledWith('/api/v1/auth/user-auth');
+    });
+
+    await waitFor(() => {
+      expect(consoleSpy).toHaveBeenCalledWith(err);
+    });
   });
 });
