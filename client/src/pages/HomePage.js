3c0876a54813c01fb553fdf9c2fde9beac15e467
George Tay <georgetay71@gmail.com>
2025-03-16 22:55:23 +0800

    test: implement ui tests for search and filter feature

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -90,12 +90,14 @@ const HomePage = () => {
   };
 
   useEffect(() => {
-    if (!checked.length || !radio.length) getAllProducts();
-  }, [checked.length, radio.length]);
-
-  useEffect(() => {
-    if (checked.length || radio.length) filterProduct();
-  }, [checked, radio]);
+    if (!checked.length && !radio.length) {
+      // get all the products if no filter is applied
+      getAllProducts();
+    } else if (checked.length || radio.length) {
+      // filter the products if either filters are applied
+      filterProduct();
+    }
+  }, [checked.length, checked, radio.length, radio]);
 
   //get filterd product
   const filterProduct = async () => {
---
+++ ui-tests/filter-search.spec.js
@@ -0,0 +1,314 @@
+import { test, expect } from '@playwright/test';
+import { describe } from 'node:test';
+import { USER } from '../config/seed/seedDb';
+
+test.beforeEach(async () => {});
+
+test.afterEach(async () => {});
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('.');
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Enter Your Email' }).click();
+  await page
+    .getByRole('textbox', { name: 'Enter Your Email' })
+    .fill(USER.email);
+  await page.getByRole('textbox', { name: 'Enter Your Password' }).click();
+  await page
+    .getByRole('textbox', { name: 'Enter Your Password' })
+    .fill(USER.password);
+  await page.getByRole('button', { name: 'LOGIN' }).click();
+});
+
+test('category filter should work', async ({ page }) => {
+  await page.goto('.');
+
+  // assert that all the products are visible
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Laptop$1,000.00A laptop is a' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Novel$10.00A novel is a' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Phone$500.00A telephone, or' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Tshirt$20.00A T-shirt, or tee' })
+      .nth(3)
+  ).toBeVisible();
+
+  // check that the search filter works properly
+  await page.locator('label').filter({ hasText: 'Books' }).click();
+
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Laptop$1,000.00A laptop is a' })
+      .nth(3)
+  ).not.toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Novel$10.00A novel is a' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Phone$500.00A telephone, or' })
+      .nth(3)
+  ).not.toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Tshirt$20.00A T-shirt, or tee' })
+      .nth(3)
+  ).not.toBeVisible();
+});
+
+test('price filter should work', async ({ page }) => {
+  await page.goto('.');
+  // assert that all the products are visible
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Laptop$1,000.00A laptop is a' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Novel$10.00A novel is a' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Phone$500.00A telephone, or' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Tshirt$20.00A T-shirt, or tee' })
+      .nth(3)
+  ).toBeVisible();
+
+  // check that the monetary filter works properly
+  await page.getByRole('radio', { name: '$20 to' }).check();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Laptop$1,000.00A laptop is a' })
+      .nth(3)
+  ).not.toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Novel$10.00A novel is a' })
+      .nth(3)
+  ).not.toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Phone$500.00A telephone, or' })
+      .nth(3)
+  ).not.toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Tshirt$20.00A T-shirt, or tee' })
+      .nth(3)
+  ).toBeVisible();
+});
+
+test('category and price filter should work', async ({ page }) => {
+  await page.goto('.');
+
+  // assert that all the products are visible
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Laptop$1,000.00A laptop is a' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Novel$10.00A novel is a' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Phone$500.00A telephone, or' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Tshirt$20.00A T-shirt, or tee' })
+      .nth(3)
+  ).toBeVisible();
+
+  // check that the filters works properly
+  await page.locator('label').filter({ hasText: 'Books' }).click();
+  await page.getByRole('radio', { name: '$20 to' }).check();
+
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Laptop$1,000.00A laptop is a' })
+      .nth(3)
+  ).not.toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Novel$10.00A novel is a' })
+      .nth(3)
+  ).not.toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Phone$500.00A telephone, or' })
+      .nth(3)
+  ).not.toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Tshirt$20.00A T-shirt, or tee' })
+      .nth(3)
+  ).not.toBeVisible();
+});
+
+test('reset filter should work', async ({ page }) => {
+  await page.goto('.');
+
+  // assert that all the products are visible
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Laptop$1,000.00A laptop is a' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Novel$10.00A novel is a' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Phone$500.00A telephone, or' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Tshirt$20.00A T-shirt, or tee' })
+      .nth(3)
+  ).toBeVisible();
+
+  // check that the monetary filter works properly
+  await page.getByRole('radio', { name: '$20 to' }).check();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Laptop$1,000.00A laptop is a' })
+      .nth(3)
+  ).not.toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Novel$10.00A novel is a' })
+      .nth(3)
+  ).not.toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Phone$500.00A telephone, or' })
+      .nth(3)
+  ).not.toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Tshirt$20.00A T-shirt, or tee' })
+      .nth(3)
+  ).toBeVisible();
+
+  // reset the filters and check that they are reset
+  await page.getByRole('button', { name: 'RESET FILTERS' }).click();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Laptop$1,000.00A laptop is a' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Novel$10.00A novel is a' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Phone$500.00A telephone, or' })
+      .nth(3)
+  ).toBeVisible();
+  await expect(
+    page
+      .getByRole('main')
+      .locator('div')
+      .filter({ hasText: 'Tshirt$20.00A T-shirt, or tee' })
+      .nth(3)
+  ).toBeVisible();
+});
