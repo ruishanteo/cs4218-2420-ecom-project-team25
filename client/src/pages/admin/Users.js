7d0bc9a421a7bd7d37840ed46dab4fa8946cf479
bernice <bernicetoh2003@gmail.com>
2025-03-16 17:02:41 +0800

    fix margin

--- client/src/pages/admin/Users.js
+++ client/src/pages/admin/Users.js
@@ -48,7 +48,7 @@ const Users = () => {
 
   return (
     <Layout title={"Dashboard - All Users"}>
-      <div className="container-fluid m-3 p-3">
+      <div className="container-fluid mt-3 mb-3 p-3">
         <div className="row">
           <div className="col-md-3">
             <AdminMenu />
