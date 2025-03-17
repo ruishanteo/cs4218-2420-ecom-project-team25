800368ccc5107dae9b6b8a2379c14db2f771d6ff
bernice <bernicetoh2003@gmail.com>
2025-03-17 21:40:13 +0800

    add substring

--- client/src/pages/admin/Products.js
+++ client/src/pages/admin/Products.js
@@ -44,10 +44,7 @@ const Products = () => {
         </div>
         <div className="col-md-9 ">
           <h1>All Products List</h1>
-          <div
-            className="d-flex flex-wrap"
-            style={{ maxHeight: "calc(100vh - 250px)", overflowY: "auto" }}
-          >
+          <div className="d-flex flex-wrap">
             {products?.map((p, index) => (
               <Link
                 key={p._id}
@@ -62,7 +59,9 @@ const Products = () => {
                   />
                   <div className="card-body">
                     <h5 className="card-title">{p.name}</h5>
-                    <p className="card-text">{p.description}</p>
+                    <p className="card-text">
+                      {p.description.substring(0, 60)}...
+                    </p>
                   </div>
                 </div>
               </Link>
