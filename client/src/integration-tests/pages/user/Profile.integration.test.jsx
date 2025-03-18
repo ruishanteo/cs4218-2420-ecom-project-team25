e8977178096ce9eef4b71726a8bb3eb3929ede2a
bernice <bernicetoh2003@gmail.com>
2025-03-18 16:11:50 +0800

    add profile frontend integration tests

--- client/src/integration-tests/pages/user/Profile.integration.test.jsx
+++ client/src/integration-tests/pages/user/Profile.integration.test.jsx
@@ -16,8 +16,20 @@ import Profile from "../../../pages/user/Profile";
 import userEvent from "@testing-library/user-event";
 import toast from "react-hot-toast";
 
-jest.spyOn(toast, "success");
+// integration tests:
+// - should perform actions a user would do. For example: to change the state of a component, a click event would be fired.
+// - should be kept minimal
+// - always test behaviour and not implementation
+
 jest.mock("axios");
+jest.spyOn(toast, "success");
+jest.spyOn(toast, "error");
+
+jest.mock("../../../hooks/useCategory", () => ({
+  __esModule: true,
+  default: jest.fn(() => [[], jest.fn()]),
+}));
+
 jest.mock("../../../context/auth", () => ({
   useAuth: jest.fn(),
 }));
@@ -62,78 +74,164 @@ describe("Profile Integration Tests", () => {
     );
   };
 
+  const setAuthInLocalStorage = (authData) => {
+    localStorage.setItem("auth", JSON.stringify(authData));
+  };
+
   beforeEach(() => {
     jest.clearAllMocks();
-    useAuth.mockReturnValue(
-      [
-        {
-          user: mockUser,
-        },
-      ],
-      jest.fn()
+
+    useAuth.mockReturnValue([
+      {
+        user: mockUser,
+      },
+      jest.fn(),
+    ]);
+  });
+
+  it("should display correct user details", async () => {
+    setAuthInLocalStorage({ user: mockUser, token: "valid-token" });
+    setup();
+
+    expect(
+      screen.getByRole("button", { bame: mockUser.name })
+    ).toBeInTheDocument();
+
+    expect(screen.getByPlaceholderText("Enter Your Name").value).toEqual(
+      mockUser.name
+    );
+    expect(screen.getByPlaceholderText("Enter Your Email").value).toEqual(
+      mockUser.email
+    );
+    expect(screen.getByPlaceholderText("Enter Your Phone").value).toEqual(
+      mockUser.phone
+    );
+    expect(screen.getByPlaceholderText("Enter Your Address").value).toEqual(
+      mockUser.address
     );
   });
 
-  it("should render the right layour component", async () => {
+  it("should submit form successfully when user updates profile", async () => {
+    setAuthInLocalStorage({ user: mockUser, token: "valid-token" });
+    const updatedUser = {
+      name: "John Doeee",
+      email: "john@gmail.com",
+      address: "NTU, Singapore",
+      phone: "123456789100",
+    };
+    axios.put.mockResolvedValue({ data: { error: null, updatedUser } });
+
     setup();
+
+    // simulate user typing in the input fields
+    // must clear the input field before typing
+    await act(async () => {
+      await user.clear(screen.getByPlaceholderText("Enter Your Name"));
+      await user.type(
+        screen.getByPlaceholderText("Enter Your Name"),
+        updatedUser.name
+      );
+
+      await user.type(
+        screen.getByPlaceholderText("Enter Your Password"),
+        "newpassword"
+      );
+      await user.clear(screen.getByPlaceholderText("Enter Your Phone"));
+      await user.type(
+        screen.getByPlaceholderText("Enter Your Phone"),
+        updatedUser.phone
+      );
+      await user.clear(screen.getByPlaceholderText("Enter Your Address"));
+      await user.type(
+        screen.getByPlaceholderText("Enter Your Address"),
+        updatedUser.address
+      );
+    });
+
+    // fire event submmision of button
+    fireEvent.click(screen.getByRole("button", { name: /UPDATE/i }));
+
     await waitFor(() => {
-      expect(document.title).toBe("Your Profile");
+      expect(axios.put).toHaveBeenCalled();
+    });
+
+    await waitFor(() => {
+      expect(toast.success).toHaveBeenCalledWith(
+        "Profile Updated Successfully"
+      );
+    });
+    // check that form inputs are updated
+    expect(screen.getByPlaceholderText("Enter Your Name").value).toEqual(
+      updatedUser.name
+    );
+    expect(screen.getByPlaceholderText("Enter Your Phone").value).toEqual(
+      updatedUser.phone
+    );
+    expect(screen.getByPlaceholderText("Enter Your Address").value).toEqual(
+      updatedUser.address
+    );
+    expect(screen.getByPlaceholderText("Enter Your Email").value).toEqual(
+      mockUser.email
+    );
+  });
+
+  it("should show error on api error", async () => {
+    setAuthInLocalStorage({ user: mockUser, token: "valid-token" });
+
+    axios.put.mockResolvedValueOnce({
+      data: { error: "API error", updatedUser: null },
+    });
+
+    setup();
+
+    // simulate user typing in the input fields
+    // must clear the input field before typing
+    await act(async () => {
+      await user.clear(screen.getByPlaceholderText("Enter Your Name"));
+      await user.type(
+        screen.getByPlaceholderText("Enter Your Name"),
+        "John Doeee"
+      );
+    });
+
+    // fire event submmision of button
+    fireEvent.click(screen.getByRole("button", { name: /UPDATE/i }));
+
+    await waitFor(() => {
+      expect(axios.put).toHaveBeenCalled();
+    });
+
+    await waitFor(() => {
+      expect(toast.error).toHaveBeenCalledWith("API error");
     });
   });
 
-  it("should render user menu", async () => {
+  it("should show error on server error", async () => {
+    setAuthInLocalStorage({ user: mockUser, token: "valid-token" });
+    // axios put throw error
+    axios.put.mockRejectedValueOnce(new Error("Server error"));
+
     setup();
-    await waitFor(() => {
-      expect(screen.getByText("Profile")).toBeInTheDocument();
+
+    // simulate user typing in the input fields
+    // must clear the input field before typing
+    await act(async () => {
+      await user.clear(screen.getByPlaceholderText("Enter Your Name"));
+      await user.type(
+        screen.getByPlaceholderText("Enter Your Name"),
+        "John Doeee"
+      );
     });
+
+    // fire event submmision of button
+    fireEvent.click(screen.getByRole("button", { name: /UPDATE/i }));
+
     await waitFor(() => {
-      expect(screen.getByText("Orders")).toBeInTheDocument();
+      expect(axios.put).toHaveBeenCalled();
+    });
+
+    await waitFor(() => {
+      expect(toast.error).toHaveBeenCalledWith("Something went wrong");
     });
   });
-
-  it("should render user's name in dropdown", async () => {
-    setup();
-    await waitFor(() => {
-      expect(
-        screen.getByRole("button", { name: "John Doe" })
-      ).toBeInTheDocument();
-    });
-  });
-
-  //   it("should submit profile form successfully", async () => {
-  //     const updatedUser = {
-  //       name: "John Doeee",
-  //       email: "john@gmail.com",
-  //       address: "NTU, Singapore",
-  //       phone: "123456789100",
-  //     };
-  //     axios.put.mockResolvedValue({ data: { error: null, updatedUser } });
-
-  //     setup();
-
-  //     await act(async () => {
-  //       await user.type(
-  //         screen.getByPlaceholderText("Enter Your Name"),
-  //         updatedUser.name
-  //       );
-  //       await user.type(
-  //         screen.getByPlaceholderText("Enter Your Password"),
-  //         "newpassword"
-  //       );
-  //       await user.type(
-  //         screen.getByPlaceholderText("Enter Your Phone"),
-  //         updatedUser.phone
-  //       );
-  //       await user.type(
-  //         screen.getByPlaceholderText("Enter Your Address"),
-  //         updatedUser.address
-  //       );
-  //     });
-
-  //     fireEvent.click(screen.getByRole("button", { name: /UPDATE/i }));
-
-  //     await waitFor(() => {
-  //       expect(axios.put).toHaveBeenCalled();
-  //     });
-  //   });
 });
